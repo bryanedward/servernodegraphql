d21c554fed5750bd6400857268ce6f0e1dd6121b
edwardev <edwardbrian96@gmail.com>
2021-09-27 10:45:35 -0500

    ssl

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
 const { Client } = require('pg');
 require('dotenv').config()
 
+
 const client = new Client({
     user: process.env.User,
     host: process.env.Host,
     database: process.env.Database,
-    password: process.env.Password
+    password: process.env.Password,
+    ssl: process.env.DATABASE_URL ? true : false
 });
 
 
